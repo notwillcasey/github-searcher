d74ca6aa67f5e451359574a2b9ce9c161c0493bd
William Casey <notwillcasey@gmail.com>
2022-01-10 22:55:53 -0700

    form skeleton init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import './App.css';
 
 function App() {
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const query = {
+      type: e.target.type.value,
+      username: e.target.username.value,
+      repo: e.target.repo.value
+    }
+    console.log(query);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <div>GitHub Searcher</div>
       </header>
 
-      <div>This is where we will search for the GitHub data</div>
+      <div className="App-body">
+
+        <form className="search-form" onSubmit={handleSearch}>
+
+          <label for="type">Select Search Type:</label>
+          <select name="type">
+            <option value="repos">Repositories</option>
+            <option value="open-requests">Open Pull Requests</option>
+            <option value="open-requests-commits">Open Pull Requests + Number of Commits</option>
+          </select><br/>
+
+          <label for="username">username:</label>
+          <input type='text' name="username"></input><br/>
+
+          <label for="repo">repo:</label>
+          <input type='text' name="repo"></input><br/>
+
+          <button type="submit">search</button>
+        </form>
+
+      </div>
     </div>
   );
 }
