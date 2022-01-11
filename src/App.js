2908367d3dd1fd11bbebf28a1a4623d77fdb8106
William Casey <notwillcasey@gmail.com>
2022-01-10 22:05:30 -0700

    create-react-app React skeleton init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>GitHub Searcher</div>
+      </header>
+
+      <div>This is where we will search for the GitHub data</div>
+    </div>
+  );
+}
+
+export default App;
